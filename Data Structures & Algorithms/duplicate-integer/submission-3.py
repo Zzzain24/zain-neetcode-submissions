cddc46036fed98f26948b97497e94b2307c65ad9
Zain Bharde <105802338+Zzzain24@users.noreply.github.com>
2026-08-12 17:41:17 -0700

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # loop through the numbers
+        # keep a list to track each number's occurence in the list
+        # if a number already exists in this ocurrence list, return True
+        # if reach outside the loop return False
+        dups = []
+        for n in nums:
+            if n in dups:
+                return True
+            dups.append(n)
+        return False 
